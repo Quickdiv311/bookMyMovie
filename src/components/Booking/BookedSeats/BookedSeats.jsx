78010c9a57fc005a663f2b287a29a9e6953f90cf
Divyansh Agarwal <dagarwal058@gmail.com>
2023-03-16 08:59:53 +0530

    total price

--- src/components/Booking/BookedSeats/BookedSeats.jsx
+++ src/components/Booking/BookedSeats/BookedSeats.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { bookingSelector } from '../../../store/reducers/BookingReducer';
+import { bookingSelector, priceSelector } from '../../../store/reducers/BookingReducer';
 import styles from './BookedSeats.module.css';
 
 const BookedSeats = () => {
   const selectedSeats = useSelector(bookingSelector);
+  const seatsPrice = useSelector(priceSelector);
 
   return (
-    <div className={styles.container}>
-        <div className={styles.left}>
-            <h5>You have Chosen Seat</h5>
-            <h1>
-              {
-                selectedSeats.map((seat) => <span>{seat+" "}</span>)
-              }
-            </h1>
-        </div>
-        <div className={styles.left}>
-            <h5>Total Price</h5>
-            <h1>150</h1>
-        </div>
-        <button className={styles.rightBtn}>PROCEED</button>
+    <div className={styles.bookingTotal}>
+      { selectedSeats.length>0 &&
+       <div className={styles.container}>
+      <div className={styles.left}>
+          <h5>You have Chosen Seat</h5>
+          <h1>
+            {
+              selectedSeats.map((seat) => <span>{seat+" "}</span>)
+            }
+          </h1>
+      </div>
+      <div className={styles.left}>
+          <h5>Total Price</h5>
+          <h1>{seatsPrice}</h1>
+      </div>
+      <button className={styles.rightBtn}>PROCEED</button>
+      </div>}
     </div>
   );
 }
--- src/store/reducers/BookingReducer.js
+++ src/store/reducers/BookingReducer.js
@@ -4,22 +4,26 @@ const bookingSlice = createSlice({
     name: 'booking',
     initialState: {
         selectedSeats: [],
-        seatPrice: 150
+        seatPrice: 0
     },
     reducers: {
         select:(state,action)=>{
             state.selectedSeats.push(action.payload);
+            state.seatPrice += 150;
         },
         remove:(state,action)=>{
             state.selectedSeats.splice(state.selectedSeats.indexOf(action.payload),1);
+            state.seatPrice -= 150;
         },
         selectDouble:(state,action)=>{
             state.selectedSeats.push(action.payload[0]);
             state.selectedSeats.push(action.payload[1]);
+            state.seatPrice += 400;
         },
         removeDouble:(state,action)=>{
             state.selectedSeats.splice(state.selectedSeats.indexOf(action.payload[0]),1);
             state.selectedSeats.splice(state.selectedSeats.indexOf(action.payload[1]),1);
+            state.seatPrice -= 400;
         }
     }
 })
@@ -27,3 +31,4 @@ const bookingSlice = createSlice({
 export const bookingReducer = bookingSlice.reducer;
 export const {select,remove,selectDouble,removeDouble} = bookingSlice.actions;
 export const bookingSelector = (state) => state.bookingReducer.selectedSeats;
+export const priceSelector = (state) => state.bookingReducer.seatPrice;
